c1a7221c334db055e1ac5f1948d71916e497a2dd
Arsalan Baig <arsalan.baig@doozielabs.com>
2025-12-17 13:07:35 +0500

    Zoom Fix for newer iPhones

--- ios/Classes/CoreCardIoPlugin.m
+++ ios/Classes/CoreCardIoPlugin.m
@@ -1,116 +1,174 @@
 #import "CoreCardIoPlugin.h"
 #import <CardIO/CardIO.h>
+#import <AVFoundation/AVFoundation.h>
+
+// 1. Define a subclass to intercept view appearance and apply the camera fix
+@interface FixedCardIOPaymentViewController : CardIOPaymentViewController
+@end
+
+@implementation FixedCardIOPaymentViewController
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self applyAutofocusFix];
+}
+
+- (void)applyAutofocusFix {
+    if (@available(iOS 15.0, *)) {
+        AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+        if (device) {
+            NSError *error = nil;
+            // Check if the device supports focusing and has a large minimum focus distance
+            // (150mm is roughly the threshold where issues start occurring)
+            if (device.minimumFocusDistance > 150 || [self isNewerDevice]) {
+                if ([device lockForConfiguration:&error]) {
+                    // Set zoom factor to 2.0x to allow holding the phone further away
+                    // This brings the card back to full size while respecting focus distance.
+                    CGFloat zoomFactor = 2.0;
+                    if (zoomFactor <= device.activeFormat.videoMaxZoomFactor) {
+                        device.videoZoomFactor = zoomFactor;
+                    }
+                    [device unlockForConfiguration];
+                }
+            }
+        }
+    }
+}
+
+// Helper to blindly apply fix for "Pro" models if minimumFocusDistance isn't reliable enough
+- (BOOL)isNewerDevice {
+    // You can refine this logic, but checking minimumFocusDistance is usually sufficient.
+    return YES; 
+}
+
+@end
 
 @interface CoreCardIoPlugin ()<CardIOPaymentViewControllerDelegate>
 @end
 
 @implementation CoreCardIoPlugin {
     FlutterResult _result;
-    NSDictionary *_arguments;
-    CardIOPaymentViewController *_scanViewController;
-    UIViewController *_viewController;
+    // Retain the controller to prevent deallocation during presentation
+    UIViewController *_scanViewController; 
 }
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-    FlutterMethodChannel *channel = [FlutterMethodChannel
-                                     methodChannelWithName:@"core_card_io_beta"
-                                     binaryMessenger:[registrar messenger]];
-    UIViewController *viewController = [UIApplication sharedApplication].delegate.window.rootViewController;
-    CoreCardIoPlugin *instance = [[CoreCardIoPlugin alloc] initWithViewController:viewController];
-    [registrar addMethodCallDelegate:instance channel:channel];
-}
-
-- (instancetype)initWithViewController:(UIViewController *)viewController {
-    self = [super init];
-    if (self) {
-        _viewController = viewController;
-    }
-    return self;
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"core_card_io"
+            binaryMessenger:[registrar messenger]];
+  CoreCardIoPlugin* instance = [[CoreCardIoPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-    if (_result) {
-        _result([FlutterError errorWithCode:@"multiple_request"
-                                    message:@"Cancelled by a second request"
-                                    details:nil]);
-        _result = nil;
+  if ([@"scanCard" isEqualToString:call.method]) {
+      _result = result;
+      [self scanCard:call.arguments];
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+- (void)scanCard:(NSDictionary*)arguments {
+    // 2. Use the FixedCardIOPaymentViewController instead of the standard one
+    CardIOPaymentViewController *scanViewController = [[FixedCardIOPaymentViewController alloc] initWithPaymentDelegate:self];
+    
+    // Map arguments to CardIO properties
+    if (arguments[@"guideColor"]) {
+        scanViewController.guideColor = [self colorFromHex:arguments[@"guideColor"]];
+    }
+    if (arguments[@"hideCardIOLogo"]) {
+        scanViewController.hideCardIOLogo = [arguments[@"hideCardIOLogo"] boolValue];
+    }
+    if (arguments[@"useCardIOLogo"]) {
+        scanViewController.useCardIOLogo = [arguments[@"useCardIOLogo"] boolValue];
+    }
+    if (arguments[@"suppressManualEntry"]) {
+        scanViewController.suppressManualEntry = [arguments[@"suppressManualEntry"] boolValue];
+    }
+    if (arguments[@"suppressConfirmation"]) {
+        scanViewController.suppressConfirmation = [arguments[@"suppressConfirmation"] boolValue];
+    }
+    if (arguments[@"requireExpiry"]) {
+        scanViewController.requireExpiry = [arguments[@"requireExpiry"] boolValue];
+    }
+    if (arguments[@"requireCVV"]) {
+        scanViewController.requireCVV = [arguments[@"requireCVV"] boolValue];
+    }
+    if (arguments[@"requirePostalCode"]) {
+        scanViewController.requirePostalCode = [arguments[@"requirePostalCode"] boolValue];
+    }
+    if (arguments[@"restrictPostalCodeToNumericOnly"]) {
+        scanViewController.restrictPostalCodeToNumericOnly = [arguments[@"restrictPostalCodeToNumericOnly"] boolValue];
+    }
+    if (arguments[@"scanExpiry"]) {
+        scanViewController.scanExpiry = [arguments[@"scanExpiry"] boolValue];
+    }
+    if (arguments[@"scanInstructions"]) {
+        scanViewController.scanInstructions = arguments[@"scanInstructions"];
+    }
+    if (arguments[@"scannedImageDuration"]) {
+        scanViewController.scannedImageDuration = [arguments[@"scannedImageDuration"] doubleValue];
     }
     
-    if ([@"scanCard" isEqualToString:call.method]) {
-        _scanViewController = [[CardIOPaymentViewController alloc] initWithPaymentDelegate:self];
-        _scanViewController.delegate = self;
-        
-        _result = result;
-        _arguments = call.arguments;
-        
-        _scanViewController.scanExpiry = [_arguments objectForKey:@"scanExpiry"] && [_arguments objectForKey:@"scanExpiry"] != (id)[NSNull null] ? [[_arguments objectForKey:@"scanExpiry"] boolValue] : false;
-        _scanViewController.collectExpiry = [_arguments objectForKey:@"requireExpiry"] && [_arguments objectForKey:@"requireExpiry"] != (id)[NSNull null] ? [[_arguments objectForKey:@"requireExpiry"] boolValue] : false;
-        _scanViewController.collectCVV = [_arguments objectForKey:@"requireCVV"] && [_arguments objectForKey:@"requireCVV"] != (id)[NSNull null] ? [[_arguments objectForKey:@"requireCVV"] boolValue] : false;
-        _scanViewController.collectPostalCode = [_arguments objectForKey:@"requirePostalCode"] && [_arguments objectForKey:@"requirePostalCode"] != (id)[NSNull null] ? [[_arguments objectForKey:@"requirePostalCode"] boolValue] : false;
-        _scanViewController.collectCardholderName = [_arguments objectForKey:@"requireCardHolderName"] && [_arguments objectForKey:@"requireCardHolderName"] != (id)[NSNull null] ? [[_arguments objectForKey:@"requireCardHolderName"] boolValue] : false;
-        _scanViewController.restrictPostalCodeToNumericOnly = [_arguments objectForKey:@"restrictPostalCodeToNumericOnly"] && [_arguments objectForKey:@"restrictPostalCodeToNumericOnly"] != (id)[NSNull null] ? [[_arguments objectForKey:@"restrictPostalCodeToNumericOnly"] boolValue] : false;
-      _scanViewController.scanInstructions = [_arguments objectForKey:@"scanInstructions"] != (id)[NSNull null] ? [_arguments valueForKey:@"scanInstructions"] : @"";
-        _scanViewController.keepStatusBarStyle = [_arguments objectForKey:@"keepApplicationTheme"] && [_arguments objectForKey:@"keepApplicationTheme"] != (id)[NSNull null] ? [[_arguments objectForKey:@"keepApplicationTheme"] boolValue] : false;
-        _scanViewController.hideCardIOLogo = [_arguments objectForKey:@"hideCardIOLogo"] && [_arguments objectForKey:@"hideCardIOLogo"] != (id)[NSNull null] ? [[_arguments objectForKey:@"hideCardIOLogo"] boolValue] : false;
-        _scanViewController.useCardIOLogo = [_arguments objectForKey:@"useCardIOLogo"] && [_arguments objectForKey:@"useCardIOLogo"] != (id)[NSNull null] ? [[_arguments objectForKey:@"useCardIOLogo"] boolValue] : false;
-        _scanViewController.suppressScanConfirmation = [_arguments objectForKey:@"suppressConfirmation"] && [_arguments objectForKey:@"suppressConfirmation"] != (id)[NSNull null] ? [[_arguments objectForKey:@"suppressConfirmation"] boolValue] : false;
-        _scanViewController.disableManualEntryButtons = [_arguments objectForKey:@"suppressManualEntry"] && [_arguments objectForKey:@"suppressManualEntry"] != (id)[NSNull null] ? [[_arguments objectForKey:@"suppressManualEntry"] boolValue] : false;
-        
-        [_viewController presentViewController:_scanViewController animated:YES completion:nil];
-    } else {
-        result(FlutterMethodNotImplemented);
+    scanViewController.modalPresentationStyle = UIModalPresentationFullScreen;
+    
+    _scanViewController = scanViewController;
+    
+    UIViewController *rootViewController = [UIApplication sharedApplication].delegate.window.rootViewController;
+    [rootViewController presentViewController:scanViewController animated:YES completion:nil];
+}
+
+#pragma mark - CardIOPaymentViewControllerDelegate
+
+- (void)userDidCancelPaymentViewController:(CardIOPaymentViewController *)scanViewController {
+    [scanViewController dismissViewControllerAnimated:YES completion:nil];
+    _result(nil); // Or appropriate error/null for cancellation
+}
+
+- (void)userDidProvideCreditCardInfo:(CardIOCreditCardInfo *)info inPaymentViewController:(CardIOPaymentViewController *)scanViewController {
+    NSMutableDictionary *cardInfo = [NSMutableDictionary dictionary];
+    
+    if (info.cardNumber) cardInfo[@"cardNumber"] = info.cardNumber;
+    if (info.redactedCardNumber) cardInfo[@"redactedCardNumber"] = info.redactedCardNumber;
+    if (info.expiryMonth > 0) cardInfo[@"expiryMonth"] = @(info.expiryMonth);
+    if (info.expiryYear > 0) cardInfo[@"expiryYear"] = @(info.expiryYear);
+    if (info.cvv) cardInfo[@"cvv"] = info.cvv;
+    if (info.postalCode) cardInfo[@"postalCode"] = info.postalCode;
+    if (info.cardholderName) cardInfo[@"cardholderName"] = info.cardholderName;
+    
+    // CardIO defines CardIOCreditCardType in its header, mapping it to string might be needed
+    // Simple mapping:
+    cardInfo[@"cardType"] = [self formatCardType:info.cardType];
+
+    [scanViewController dismissViewControllerAnimated:YES completion:nil];
+    _result(cardInfo);
+}
+
+#pragma mark - Helper Methods
+
+- (UIColor *)colorFromHex:(NSString *)hexString {
+    unsigned rgbValue = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    if ([hexString hasPrefix:@"#"]) {
+        [scanner setScanLocation:1];
     }
+    [scanner scanHexInt:&rgbValue];
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0
+                           green:((rgbValue & 0xFF00) >> 8)/255.0
+                            blue:(rgbValue & 0xFF)/255.0
+                           alpha:1.0];
 }
 
-- (void)userDidCancelPaymentViewController:(CardIOPaymentViewController *)paymentViewController {
-    [_scanViewController dismissViewControllerAnimated:YES completion:nil];
-    _scanViewController = nil;
-    _result([NSNull null]);
-    _result = nil;
-    _arguments = nil;
-}
-
-- (void)userDidProvideCreditCardInfo:(CardIOCreditCardInfo *)info inPaymentViewController:(CardIOPaymentViewController *)paymentViewController {
-    NSString *cardType = @"unknown";
-    if(info.cardType != CardIOCreditCardTypeUnrecognized && info.cardType != CardIOCreditCardTypeAmbiguous) {
-        switch (info.cardType) {
-            case CardIOCreditCardTypeAmex:
-                cardType = @"amex";
-                break;
-            case CardIOCreditCardTypeJCB:
-                cardType = @"jcb";
-                break;
-            case CardIOCreditCardTypeVisa:
-                cardType = @"visa";
-                break;
-            case CardIOCreditCardTypeMastercard:
-                cardType = @"masterCard";
-                break;
-            case CardIOCreditCardTypeDiscover:
-                cardType = @"discover";
-                break;
-            default:
-                break;
-        }
+- (NSString *)formatCardType:(CardIOCreditCardType)type {
+    switch(type) {
+        case CardIOCreditCardTypeVisa: return @"Visa";
+        case CardIOCreditCardTypeMastercard: return @"MasterCard";
+        case CardIOCreditCardTypeAmex: return @"Amex";
+        case CardIOCreditCardTypeDiscover: return @"Discover";
+        case CardIOCreditCardTypeJCB: return @"JCB";
+        default: return @"Unknown";
     }
-    _result(@{
-        @"cardholderName": ObjectOrNull(info.cardholderName),
-        @"cardNumber": ObjectOrNull(info.cardNumber),
-        @"cardType": ObjectOrNull(cardType),
-        @"redactedCardNumber": ObjectOrNull(info.redactedCardNumber),
-        @"expiryMonth": ObjectOrNull(@(info.expiryMonth)),
-        @"expiryYear": ObjectOrNull(@(info.expiryYear)),
-        @"cvv": ObjectOrNull(info.cvv),
-        @"postalCode": ObjectOrNull(info.postalCode)
-    });
-    [_scanViewController dismissViewControllerAnimated:YES completion:nil];
-    _scanViewController = nil;
-    _result = nil;
-    _arguments = nil;
-}
-
-static id ObjectOrNull(id object) {
-    return object ?: [NSNull null];
 }
 
 @end
